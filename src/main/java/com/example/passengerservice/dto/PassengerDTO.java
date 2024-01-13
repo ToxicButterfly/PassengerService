dd9daa0e6ad8b385d3f5e4aff25ad842e831d9f3
Toxic Butterfly <qwerty541683@gmail.com>
2024-01-13 14:33:24 +0300

    model+DTO

---
+++ src/main/java/com/example/passengerservice/dto/PassengerDTO.java
@@ -0,0 +1,12 @@
+package com.example.passengerservice.dto;
+
+import lombok.Data;
+
+
+@Data
+public class PassengerDTO {
+    private Integer id;
+    private String fullName;
+    private String username;
+    private String email;
+}
